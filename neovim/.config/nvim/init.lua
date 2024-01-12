ee4b5fffd58c86f37321038726ca311880d15093
Caroline Tippins <ctipp015@uottawa.ca>
2024-01-11 19:58:35 -0500

    make indent 4 tabs

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -59,6 +59,12 @@ vim.wo.number = true
 -- Make line numbers relative
 vim.wo.relativenumber = true
 
+-- Make indent 4 tabs
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
