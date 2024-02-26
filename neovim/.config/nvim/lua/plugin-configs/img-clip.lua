0559312e81e986807bdbfff42d90938c122ef95c
Caroline132 <ctipp015@uottawa.ca>
2024-02-25 19:02:17 -0500

    add img-clip plugin

---
+++ neovim/.config/nvim/lua/plugin-configs/img-clip.lua
@@ -0,0 +1,13 @@
+require("img-clip").setup({
+	filetypes = {
+		markdown = {
+			url_encode_path = true,
+			dir_path = ".attachments",
+			template = "![$CURSOR](/$FILE_PATH)",
+			use_absolute_path = false,
+			relative_template_path = false,
+		},
+	},
+})
+
+vim.keymap.set({ "n" }, "<leader>p", "<cmd>PasteImage<cr>", { desc = "Paste image into .attachments directory" })
