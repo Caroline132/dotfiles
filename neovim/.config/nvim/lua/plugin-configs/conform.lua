30c2f235ab9a9f2610d2c3535d51db6b3a79e288
Caroline Tippins <cbtippins@gmail.com>
2024-03-27 13:54:06 -0400

    add function to enable and disable formatter

--- neovim/.config/nvim/lua/plugin-configs/conform.lua
+++ neovim/.config/nvim/lua/plugin-configs/conform.lua
@@ -15,20 +15,52 @@ conform.setup({
 		graphql = { "prettier" },
 		lua = { "stylua" },
 		python = { "isort", "black" },
-		go = { "goimports", "gofmt", "golines" },
+		go = { "goimports", "gofmt" },
 		-- Use the "*" filetype to run formatters on all filetypes.
-		["*"] = { "codespell" },
+		-- ["*"] = { "codespell" },
 		-- Use the "_" filetype to run formatters on filetypes that don't
 		-- have other formatters configured.
 		["_"] = { "trim_whitespace" },
 	},
-	format_on_save = {
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 500,
-	},
+	format_on_save = function(bufnr)
+		-- Disable with a global or buffer-local variable
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 500, lsp_fallback = true }
+	end,
 })
 
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>fe",
+	":FormatDisable<CR>",
+	{ noremap = true, silent = true, desc = "Format disable" }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>fd",
+	":FormatDisable<CR>",
+	{ noremap = true, silent = true, desc = "Format disable" }
+)
 vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 	conform.format({
 		lsp_fallback = true,
