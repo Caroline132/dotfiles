f84ea00f6e6640d320f901e286d316303568ec99
Caroline Tippins <cbtippins@gmail.com>
2024-05-06 10:38:32 -0400

    chore: use nvim-surround instead of mini-surround

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -276,7 +276,7 @@ require("lazy").setup({
 			require("plugin-configs.mini-ai")
 			-- require("plugin-configs.mini-clue")
 			-- require("plugin-configs.mini-statusline")
-			require("plugin-configs.mini-surround")
+			-- require("plugin-configs.mini-surround")
 			require("plugin-configs.mini-indentscope")
 		end,
 	},
@@ -284,6 +284,16 @@ require("lazy").setup({
 		"gbprod/substitute.nvim",
 		config = function()
 			require("plugin-configs.substitute")
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
 		end,
 	},
 }, {})
