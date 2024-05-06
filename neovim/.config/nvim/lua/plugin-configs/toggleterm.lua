5fa7382f396bfe6e5ccc801b44b98164b1be4d01
Caroline Tippins <cbtippins@gmail.com>
2024-05-06 10:36:04 -0400

    feat: add ability to open toggleterm on current directory

--- neovim/.config/nvim/lua/plugin-configs/toggleterm.lua
+++ neovim/.config/nvim/lua/plugin-configs/toggleterm.lua
@@ -20,4 +20,70 @@ require("toggleterm").setup({
 })
 
 local term = require("toggleterm.terminal").Terminal
+local function get_term_index(current_id, terms)
+	local idx
+	for i, v in ipairs(terms) do
+		if v.id == current_id then
+			idx = i
+		end
+	end
+	return idx
+end
+local function go_prev_term()
+	local current_id = vim.b.toggle_number
+	if current_id == nil then
+		return
+	end
+
+	local terms = require("toggleterm.terminal").get_all(true)
+	local prev_index
+
+	local index = get_term_index(current_id, terms)
+	if index > 1 then
+		prev_index = index - 1
+	else
+		prev_index = #terms
+	end
+	require("toggleterm").toggle(terms[index].id)
+	require("toggleterm").toggle(terms[prev_index].id)
+end
+
+local function go_next_term()
+	local current_id = vim.b.toggle_number
+	if current_id == nil then
+		return
+	end
+
+	local terms = require("toggleterm.terminal").get_all(true)
+	local next_index
+
+	local index = get_term_index(current_id, terms)
+	if index == #terms then
+		next_index = 1
+	else
+		next_index = index + 1
+	end
+	require("toggleterm").toggle(terms[index].id)
+	require("toggleterm").toggle(terms[next_index].id)
+end
+vim.keymap.set("n", "<leader>tc", function()
+	local t = term:new({
+		dir = "%:p:h",
+		display_name = vim.fn.expand("%:p:h"),
+	})
+	t:toggle()
+end, { desc = "Toggle term current dir" })
+vim.keymap.set("n", "<leader>tt;", function()
+	local t = term:new({
+		display_name = "default",
+	})
+	t:toggle()
+end, { desc = "Create term" })
+vim.keymap.set({ "n", "t" }, "<F8>", function()
+	go_next_term()
+end, { desc = "Toggle term" })
+
+vim.keymap.set({ "n", "t" }, "<F7>", function()
+	go_prev_term()
+end, { desc = "Toggle term" })
 vim.keymap.set({ "n", "t" }, "<F9>", "<cmd>ToggleTerm<cr>", { desc = "Toggle term" })
