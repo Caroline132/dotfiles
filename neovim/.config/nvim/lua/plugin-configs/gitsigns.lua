d25477c15c4cd0bf685f3d9c6a9cc1e22ff78c98
Caroline Tippins <cbtippins@gmail.com>
2024-06-20 10:41:47 -0400

    fix: remove deprecated commands

--- neovim/.config/nvim/lua/plugin-configs/gitsigns.lua
+++ neovim/.config/nvim/lua/plugin-configs/gitsigns.lua
@@ -1,46 +1,47 @@
 require("gitsigns").setup({
-  signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "-", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "-", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-  },
-  signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  attach_to_untracked = true,
-  watch_gitdir = {
-    follow_files = true
-  },
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
-    ignore_whitespace = false,
-  },
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  max_file_length = 40000,
-  preview_config = {
-    -- Options passed to nvim_open_win
-    border = "single",
-    style = "minimal",
-    relative = "cursor",
-    row = 0,
-    col = 1,
-  },
-  yadm = {
-    enable = false,
-  },
+	signs = {
+		add = { text = "┃" },
+		change = { text = "┃" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
+	},
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+	watch_gitdir = {
+		follow_files = true,
+	},
+	auto_attach = true,
+	attach_to_untracked = false,
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 1000,
+		ignore_whitespace = false,
+		virt_text_priority = 100,
+	},
+	current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+	sign_priority = 6,
+	update_debounce = 100,
+	status_formatter = nil, -- Use default
+	max_file_length = 40000, -- Disable if file is longer than this (in lines)
+	preview_config = {
+		-- Options passed to nvim_open_win
+		border = "single",
+		style = "minimal",
+		relative = "cursor",
+		row = 0,
+		col = 1,
+	},
 })
-vim.keymap.set("n", "<leader>ha", '<cmd>Gitsigns attach<CR>', { desc = "Gitsigns attach" })
+vim.keymap.set("n", "<leader>ha", "<cmd>Gitsigns attach<CR>", { desc = "Gitsigns attach" })
 vim.keymap.set("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>', { desc = "blame line" })
-vim.keymap.set("n", "<leader>hc", '<cmd>Gitsigns toggle_current_line_blame<CR>', { desc = "toggle current line blame" })
-vim.keymap.set("n", "<leader>hl", '<cmd>Gitsigns toggle_linehl<CR>', { desc = "toggle line hl" })
+vim.keymap.set("n", "<leader>hc", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "toggle current line blame" })
+vim.keymap.set("n", "<leader>hl", "<cmd>Gitsigns toggle_linehl<CR>", { desc = "toggle line hl" })
 vim.keymap.set("n", "]h", "<cmd>Gitsigns next_hunk<CR>", { desc = "next hunk" })
 vim.keymap.set("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "prev hunk" })
 vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "preview hunk" })
