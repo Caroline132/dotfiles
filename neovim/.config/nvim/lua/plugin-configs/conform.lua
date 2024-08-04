8ba7511206e2845ff3a4a7873efa4087ca42c330
Caroline Tippins <cbtippins@gmail.com>
2024-08-04 18:57:10 -0400

    chore: add keymap to toggle formatting instead of enable/disable

--- neovim/.config/nvim/lua/plugin-configs/conform.lua
+++ neovim/.config/nvim/lua/plugin-configs/conform.lua
@@ -38,40 +38,20 @@ conform.formatters.shfmt = {
 	-- { "-i", "2", "-filename", "$FILENAME" }
 }
 
-vim.api.nvim_create_user_command("FormatDisable", function(args)
-	if args.bang then
+vim.api.nvim_create_user_command("FormatToggle", function(args)
+	if vim.b.disable_autoformat == true then
 		-- FormatDisable! will disable formatting just for this buffer
-		vim.b.disable_autoformat = true
+		vim.b.disable_autoformat = false
 	else
-		vim.g.disable_autoformat = true
+		vim.b.disable_autoformat = true
 	end
 end, {
-	desc = "Disable autoformat-on-save",
-	bang = true,
-})
-vim.api.nvim_create_user_command("FormatEnable", function()
-	vim.b.disable_autoformat = false
-	vim.g.disable_autoformat = false
-end, {
-	desc = "Re-enable autoformat-on-save",
+	desc = "Toggle autoformat-on-save",
 })
 
 vim.api.nvim_set_keymap(
 	"n",
-	"<Leader>fe",
-	":FormatDisable<CR>",
-	{ noremap = true, silent = true, desc = "Format disable" }
+	"<Leader>tf",
+	":FormatToggle<CR>",
+	{ noremap = true, silent = true, desc = "Toggle format" }
 )
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>fd",
-	":FormatDisable<CR>",
-	{ noremap = true, silent = true, desc = "Format disable" }
-)
-vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-	conform.format({
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 500,
-	})
-end, { desc = "Format file or range (in visual mode)" })
