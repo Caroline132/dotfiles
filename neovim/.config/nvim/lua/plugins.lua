80f540776fe5dcd690e1def63c9bd3a43e3ad24e
Caroline Tippins <ctipp015@uottawa.ca>
2024-09-02 18:39:42 -0400

    feat: add supermaven

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -325,15 +325,27 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
+		"supermaven-inc/supermaven-nvim",
 		config = function()
-			require("codeium").setup({})
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<C-f>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-j>",
+				},
+			})
 		end,
 	},
+	-- {
+	-- 	"Exafunction/codeium.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"hrsh7th/nvim-cmp",
+	-- 	},
+	-- 	config = function()
+	-- 		require("codeium").setup({})
+	-- 	end,
+	-- },
 	-- Themes
 	{
 		"folke/tokyonight.nvim",
