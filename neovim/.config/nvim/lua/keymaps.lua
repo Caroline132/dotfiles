fd7c39b376ba8cf4eec9d2ff0a905785f860f2ea
Caroline Tippins <cbtippins@gmail.com>
2025-02-17 20:54:21 -0500

    fix: imprive diff function

--- neovim/.config/nvim/lua/keymaps.lua
+++ neovim/.config/nvim/lua/keymaps.lua
@@ -33,69 +33,66 @@ vim.keymap.set("i", "jk", "<esc>", { desc = "escape" })
 
 -- Diffs between two selected buffers
 local pick_buffer = function(callback)
-    require("telescope.builtin").buffers({
-        attach_mappings = function(_, map)
-            map("i", "<CR>", function(prompt_bufnr)
-                local selection = require("telescope.actions.state").get_selected_entry()
-                local bufnr = selection.bufnr
-                -- Close the picker and call the callback with the selected buffer
-                require("telescope.actions").close(prompt_bufnr)
-                callback(bufnr)
-            end)
-            return true
-        end,
-    })
+	require("telescope.builtin").buffers({
+		attach_mappings = function(_, map)
+			map("i", "<CR>", function(prompt_bufnr)
+				local selection = require("telescope.actions.state").get_selected_entry()
+				local bufnr = selection.bufnr
+				-- Close the picker and call the callback with the selected buffer
+				require("telescope.actions").close(prompt_bufnr)
+				callback(bufnr)
+			end)
+			return true
+		end,
+	})
 end
 
---- Get list of active buffers from the current list of windows
----@param windows number[]
----@return number[]
-local function get_shown_buffers(windows)
-    local buf_numbers = {}
-    for _, win in ipairs(windows) do
-        local buf = vim.api.nvim_win_get_buf(win)
-        local bufinfo = vim.fn.getbufinfo(buf)[1]
-        if bufinfo and bufinfo.hidden == 0 and bufinfo.listed == 1 then
-            buf_numbers[#buf_numbers + 1] = buf
-        end
-    end
-    return buf_numbers
+--- Get a list of all listed buffer names
+---@return string[]
+local function get_listed_buffers()
+	local buf_names = {}
+	for _, buf in ipairs(vim.fn.getbufinfo()) do
+		if buf.listed == 1 and buf.name ~= "" then  -- Only include buffers with names
+			buf_names[#buf_names + 1] = buf.name
+		end
+	end
+	return buf_names
 end
 
 vim.keymap.set("n", "<localleader>db", function()
-    local windows = vim.api.nvim_list_wins()
-    local buf_numbers = get_shown_buffers(windows)
+	local buf_names = get_listed_buffers()
 
-    if #buf_numbers == 2 then
-        -- If there are exactly 2 visible buffers, diff them automatically
-        vim.cmd("tabnew " .. vim.fn.fnameescape(vim.fn.getbufinfo(buf_numbers[1])[1].name))
-        vim.cmd("vertical diffsplit " .. vim.fn.fnameescape(vim.fn.getbufinfo(buf_numbers[2])[1].name))
-        vim.cmd.normal({ args = { "gg" }, bang = true })
-    else
-        local first_bufnumber, second_bufnumber
+	if 2 == 2 then
+		-- Open the first buffer in a new tab
+		vim.cmd("tabnew " .. vim.fn.fnameescape(buf_names[1]))
+		-- Open the second buffer in a vertical diff split
+		vim.cmd("vertical diffsplit " .. vim.fn.fnameescape(buf_names[2]))
+		vim.cmd.normal({ args = { "gg" }, bang = true })
+	else
+		local first_bufnumber, second_bufnumber
 
-        -- Pick the first buffer
-        pick_buffer(function(bufnr)
-            first_bufnumber = bufnr
-            -- After picking the first buffer, immediately ask for the second buffer
-            if first_bufnumber then
-                pick_buffer(function(bufnr)
-                    second_bufnumber = bufnr
-                    -- Proceed to diff if both buffers are selected
-                    if first_bufnumber and second_bufnumber then
-                        local first_buf = vim.fn.getbufinfo(first_bufnumber)[1]
-                        local second_buf = vim.fn.getbufinfo(second_bufnumber)[1]
+		-- Pick the first buffer
+		pick_buffer(function(bufnr)
+			first_bufnumber = bufnr
+			-- After picking the first buffer, immediately ask for the second buffer
+			if first_bufnumber then
+				pick_buffer(function(bufnr)
+					second_bufnumber = bufnr
+					-- Proceed to diff if both buffers are selected
+					if first_bufnumber and second_bufnumber then
+						local first_buf = vim.fn.getbufinfo(first_bufnumber)[1]
+						local second_buf = vim.fn.getbufinfo(second_bufnumber)[1]
 
-                        if first_buf and second_buf then
-                            vim.cmd("tabnew " .. vim.fn.fnameescape(first_buf.name))
-                            vim.cmd("vertical diffsplit " .. vim.fn.fnameescape(second_buf.name))
-                            vim.cmd.normal({ args = { "gg" }, bang = true })
-                        end
-                    end
-                end)
-            end
-        end)
-    end
+						if first_buf and second_buf then
+							vim.cmd("tabnew " .. vim.fn.fnameescape(first_buf.name))
+							vim.cmd("vertical diffsplit " .. vim.fn.fnameescape(second_buf.name))
+							vim.cmd.normal({ args = { "gg" }, bang = true })
+						end
+					end
+				end)
+			end
+		end)
+	end
 end, { desc = "Diff between open buffers" })
 
 -- Diffs between current windows
@@ -151,7 +148,7 @@ vim.keymap.set("n", "<localleader>dd", function()
 			vim.cmd.normal({ args = { "gg" }, bang = true })
 		end
 	end
-end, { desc = "Diff between open files" })
+end, { desc = "Diff between window files" })
 
 vim.keymap.set("x", "<localleader>dD", function()
 	vim.cmd('noau normal! "vy')
