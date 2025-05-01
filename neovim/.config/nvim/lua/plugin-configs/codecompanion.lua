210e03ec0d5a86cb5c811f4c3d63d6e197f2ab42
Caroline Tippins <cbtippins@gmail.com>
2025-05-01 14:52:52 -0400

    chore: use claude model for codecompanion

--- neovim/.config/nvim/lua/plugin-configs/codecompanion.lua
+++ neovim/.config/nvim/lua/plugin-configs/codecompanion.lua
@@ -1,9 +1,31 @@
 require("codecompanion").setup({
+  display = {
+    chat = {
+      show_settings = false,
+    },
+  },
+  adapters = {
+    opts = {
+      show_defaults = false,
+    },
+    copilot = function()
+      return require("codecompanion.adapters").extend("copilot", {
+        schema = {
+          model = {
+            default = "claude-3.5-sonnet",
+          },
+        },
+      })
+    end,
+  },
   strategies = {
     chat = {
       adapter = "copilot",
     },
     inline = {
+      adapter = "copilot",
+    },
+    agent = {
       adapter = "copilot",
     },
   },
