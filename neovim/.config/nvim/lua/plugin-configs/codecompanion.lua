45c08b79891665c8bcff0b1e9afe8338393e1be7
Caroline Tippins <cbtippins@gmail.com>
2025-07-21 15:39:24 -0400

    fix: put extension in right place

--- neovim/.config/nvim/lua/plugin-configs/codecompanion.lua
+++ neovim/.config/nvim/lua/plugin-configs/codecompanion.lua
@@ -1,4 +1,14 @@
 require("codecompanion").setup({
+  extensions = {
+    mcphub = {
+      callback = "mcphub.extensions.codecompanion",
+      opts = {
+        make_vars = true,       
+        make_slash_commands = true,
+        show_result_in_chat = true  
+      }
+    }
+  },
   display = {
     chat = {
       show_settings = false,
@@ -28,16 +38,7 @@ require("codecompanion").setup({
     agent = {
       adapter = "copilot",
     },
-    extensions = {
-      mcphub = {
-        callback = "mcphub.extensions.codecompanion",
-        opts = {
-          show_result_in_chat = true,  -- Show mcp tool results in chat
-          make_vars = true,            -- Convert resources to #variables
-          make_slash_commands = true,  -- Add prompts as /slash commands
-        }
-      }
-    } },
+  },
 })
 
 vim.keymap.set({"i", "n", "x"}, "<leader>ta", "<Cmd>CodeCompanionChat toggle<CR>", { desc = "Toggle CodeCompanionChat"})
